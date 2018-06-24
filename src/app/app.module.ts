5984b920bf9defe4ae9c986d3b10bbc0488de8c3
Michael Goodnow <mmgoodnow@gmail.com>
2018-06-24 11:21:27 -0400

    format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
-import {BrowserModule} from "@angular/platform-browser";
-import {NgModule} from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import {AppComponent} from "./app.component";
-import {SearchResultsComponent} from "./search-results/search-results.component";
-import { RankingsComponent } from './rankings/rankings.component';
+import { AppComponent } from "./app.component";
+import { SearchResultsComponent } from "./search-results/search-results.component";
+import { RankingsComponent } from "./rankings/rankings.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SearchResultsComponent,
-    RankingsComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, SearchResultsComponent, RankingsComponent],
+	imports: [BrowserModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
