316551ee3dae271d0c0de24de989b80930acbd2a
Michael Goodnow <mmgoodnow@gmail.com>
2018-06-24 11:23:49 -0400

    formatting

--- src/app/rankings/rankings.component.ts
+++ src/app/rankings/rankings.component.ts
@@ -1,24 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-rankings',
-  templateUrl: './rankings.component.html',
-  styleUrls: ['./rankings.component.css']
+	selector: "app-rankings",
+	templateUrl: "./rankings.component.html",
+	styleUrls: ["./rankings.component.css"],
 })
 export class RankingsComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
+	players;
 
-  players;
+	retrieveResults() {
+		return fetch("http://api.snooker.org/?t=10&st=p&s=2018").then(
+			response => response.json()
+		);
+	}
 
-  retrieveResults() {
-    return fetch("http://api.snooker.org/?t=10&st=p&s=2018")
-      .then(response => response.json());
-  }
-
-  ngOnInit() {
-    this.retrieveResults()
-      .then(json => this.players = json);
-  }
-
+	ngOnInit() {
+		this.retrieveResults().then(json => (this.players = json));
+	}
 }
